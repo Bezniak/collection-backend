d106d6659056c144819baee1823caffeb6d570d0
Bezniak <ivan.bezniak@gmail.com>
2024-05-30 12:49:32 +0300

    final

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,11 @@
-module.exports = ({ env }) => ({
+require('dotenv').config();
+
+module.exports = ({env}) => ({
   meilisearch: {
     config: {
-      host: env('MEILISEARCH_HOST', 'http://localhost:7700'),
-      apiKey: env('MEILISEARCH_API_KEY', 'masterKey'),
+      host: env('MEILISEARCH_HOST', process.env.MEILISEARCH_HOST),
+      apiKey: env('MEILISEARCH_API_KEY', process.env.MEILISEARCH_API_KEY),
     },
   },
 });
+
