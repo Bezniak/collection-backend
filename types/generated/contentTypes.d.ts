b16a6ccaa27782206706824d5b90a4eab7f651b9
Bezniak <ivan.bezniak@gmail.com>
2024-06-14 18:10:46 +0300

    package.json

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -894,6 +894,12 @@ export interface ApiCollectionCollection extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.user'
     >;
+    jiraTicketId: Attribute.Text &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -1137,6 +1143,88 @@ export interface ApiTagTag extends Schema.CollectionType {
   };
 }
 
+export interface ApiTicketTicket extends Schema.CollectionType {
+  collectionName: 'tickets';
+  info: {
+    singularName: 'ticket';
+    pluralName: 'tickets';
+    displayName: 'Ticket';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  pluginOptions: {
+    i18n: {
+      localized: true;
+    };
+  };
+  attributes: {
+    summary: Attribute.Text &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    description: Attribute.Text &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    priority: Attribute.Enumeration<['High', 'Medium', 'Low']> &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    user: Attribute.Relation<
+      'api::ticket.ticket',
+      'oneToOne',
+      'plugin::users-permissions.user'
+    >;
+    collection: Attribute.Relation<
+      'api::ticket.ticket',
+      'oneToOne',
+      'api::collection.collection'
+    >;
+    link: Attribute.Text &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    status: Attribute.Enumeration<
+      ['Open', 'In Progress', 'Rejected', 'Resolved']
+    > &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::ticket.ticket',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::ticket.ticket',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    localizations: Attribute.Relation<
+      'api::ticket.ticket',
+      'oneToMany',
+      'api::ticket.ticket'
+    >;
+    locale: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -1160,6 +1248,7 @@ declare module '@strapi/types' {
       'api::item.item': ApiItemItem;
       'api::like.like': ApiLikeLike;
       'api::tag.tag': ApiTagTag;
+      'api::ticket.ticket': ApiTicketTicket;
     }
   }
 }
